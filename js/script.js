7e0b631ec7993c6f44639db7fd048a07ec374e7b
JairJG21 <gjair4267@gmail.com>
2022-09-24 10:05:39 -0500

    Desarrollo index

--- js/script.js
+++ js/script.js
@@ -1,43 +1,47 @@
 $(document).ready(function () {
-
-    var ghpages = require('gh-pages');
-
-    ghpages.publish('dist', function(err) {});
-
-
     anime({
         targets: '.nombre_principal',
         translateY: [
-            { value: 0, duration: 500 },
-            { value: 20, duration: 1000 },
-            { value: 0, duration: 1000}
+            { value: 20, duration: 800, delay: 500 },
+            { value: 0, duration: 800}
           ],
+          easing: 'spring(1, 100, 10, 30)'
       });
 
       anime({
         targets: '.apellido_principal',
         translateY: [
-            { value: 0, duration: 550 },
-            { value: 20, duration: 1100 },
-            { value: 0, duration: 1100}
+            { value: 20, duration: 900, delay: 600 },
+            { value: 0, duration: 850}
           ],
+          easing: 'spring(1, 100, 10, 30)'
       });
 
       anime({
         targets: '.ocupacion_principal',
         translateY: [
-            { value: 0, duration: 600 },
-            { value: 20, duration: 1200 },
-            { value: 0, duration: 1200}
+            { value: 20, duration: 1000, delay: 700 },
+            { value: 0, duration: 900}
           ],
+          easing: 'spring(1, 100, 10, 30)'
       });
 
       anime({
         targets: '.img_mia_1',
-        translateY: [
-            { value: 0, duration: 650 },
-            { value: 20, duration: 1300 },
-            { value: 0, duration: 1300}
-          ],
+        translateY: 10,
+          easing: 'linear',
+          direction: 'alternate',
+          loop: true,
+          duration: 1000
+      });
+});
+
+$('.icon_menu').on('click', function () {
+    anime({
+        targets: '.nav-link',
+        translateX: [
+            { value: -160 },
+            { value: 0, duration: 800, delay: 500 }
+          ]
       });
 });
